5c8d9ca6084bfb6190331975696d4ae8da087c76
Hironori Akaishi <ah.gm3622@gmail.com>
2017-10-14 10:49:52 +0900

    fix

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # URLShortening
-ただひたすら短縮URLを持ってくる
+短縮URLを簡単に見つけられるから怖いよ
+
+
+## Qiita
+https://qiita.com/h-akaishi/items/00037a240fe6a61ded00
--- URLShortening_isDanger.rb
+++ URLShortening_isDanger.rb
@@ -1,21 +1,17 @@
 require 'net/http'
 
 # 短縮URL作成
-def self.generate_uri
-  list = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map { |i| i.to_a }.flatten
-  short_uri = "https://goo.gl/"+ (0...6).map { list[rand(list.length)] }.join
+def generate_uri
+  list = [*'a'..'z', *'A'..'Z', *'0'..'9']
+  short_uri = "https://goo.gl/"+ 6.times.map { list.sample }.join
 end
 
-def self.request_uri(uri)
-  begin
-    response = Net::HTTP.get_response(URI.parse(uri))
-  rescue
-    raise
-  else
-    case response
-    when Net::HTTPMovedPermanently
-      puts uri
-    end
+def request_uri(uri)
+  response = Net::HTTP.get_response(URI.parse(uri))
+
+  case response
+  when Net::HTTPMovedPermanently
+    puts uri
   end
 end
 
